2972341fcf7ec28db63fbb6872f5d4611a807fe0
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-13 11:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	 if (!s || s.indexOf(' ') === -1) {
+        return s;
+    } else {
+        // Extract the first word by finding the index of the first space
+        const firstSpaceIndex = s.indexOf(' ');
+        return s.slice(0, firstSpaceIndex);
+    }
 }
 
 // Do not change the code below
